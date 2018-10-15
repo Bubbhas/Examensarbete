127f5371f38fd27a089b82e35dc1209595ac8e77
Christoffer J. Öhrström <christoffer.j.ohrstrom@gmail.com>
2018-10-15 13:25:55 +0200

    Fixat i services

---
+++ GameOfDojan/Services/ShoePicData.cs
@@ -0,0 +1,35 @@
+﻿using GameOfDojan.Data;
+using GameOfDojan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfDojan.Services
+{
+    public class ShoePicData : IShoePicData
+    {
+        private readonly GameOfDojanDbContext _context;
+
+        public ShoePicData(GameOfDojanDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddPictureToFolder()
+        {
+
+        }
+
+        public ShoePic GetPicFromFolder(int id)
+        {
+            return 
+        } 
+
+        public ShoePic Get(int id)
+        {
+            return _context.ShoePics.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
