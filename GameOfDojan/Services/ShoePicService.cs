1f00abbf4eea41209b17c44885dd4d6d2059ba3e
Jesper Eriksson <jesper--eriksson@hotmail.com>
2018-10-15 12:58:22 +0200

    saker

---
+++ GameOfDojan/Services/ShoePicService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfDojan.Services
+{
+    public class ShoePicService
+    {
+        public ShoePicService()
+        {
+
+        }
+    }
+}
