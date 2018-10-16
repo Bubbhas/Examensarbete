1bed8d7a628f70941f8d4ec80217058ba488c364
Christoffer J. Öhrström <christoffer.j.ohrstrom@gmail.com>
2018-10-16 09:35:57 +0200

    Skapat grundläggande vyer för AiResultat och AiResponse samt profil och bildgalleri för användare

---
+++ GameOfDojan/ViewModels/AiResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfDojan.ViewModels
+{
+    public class AiResult
+    {
+        public bool Result { get; set; }
+        public double Probability { get; set; }
+    }
+}
