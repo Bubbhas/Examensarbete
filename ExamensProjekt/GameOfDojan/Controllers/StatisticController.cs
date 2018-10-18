c7d9354e9a2d43d8cfc9eaf6baf58bf3e548f743
Jesper Eriksson <jesper--eriksson@hotmail.com>
2018-10-18 13:12:44 +0200

    statController

---
+++ ExamensProjekt/GameOfDojan/Controllers/StatisticController.cs
@@ -0,0 +1,34 @@
+﻿using GameOfDojan.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfDojan.Controllers
+{
+    public class StatisticController : Controller
+    {
+        private readonly IUserData _userData;
+        private readonly UserService _userService;
+
+        public StatisticController(IUserData userData, UserService userService)
+        {
+            _userData = userData;
+            _userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetTopList()
+        {
+            return View(_userService.GetTop10UserPoints());
+        }
+
+    }
+}
+
