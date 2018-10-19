127646274d8265ee9650e68e4bb83c469e5c9f5e
Jesper Eriksson <jesper--eriksson@hotmail.com>
2018-10-19 14:48:44 +0200

    fixat en profilsida

---
+++ ExamensProjekt/GameOfDojan/Controllers/UserProfileController.cs
@@ -0,0 +1,26 @@
+﻿using GameOfDojan.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfDojan.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        private readonly IUserData _userData;
+
+        public UserProfileController(IUserData userData)
+        {
+            _userData = userData;
+        }
+        public IActionResult Index(string id)
+        {
+            var user = _userData.GetUser(id);
+
+            return View(user);
+        }
+    }
+}
